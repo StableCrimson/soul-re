844651375cd677143b15bbd4b1a5bc7a371a490c
StableCrimson <StableCrimson@outlook.com>
2026-09-11 07:55:44 -0400

    Match WALBOSB_Message

--- src/Overlays/walbosb/walbosb.c
+++ src/Overlays/walbosb/walbosb.c
@@ -1,4 +1,7 @@
 #include "Overlays/walbosb/walbosb.h"
+#include "Game/SOUND.h"
+#include "Game/MONSTER/MONAPI.h"
+#include "Game/MONSTER/MONLIB.h"
 
 // this conditional is for the objdiff report
 #ifndef SKIP_ASM
@@ -19,7 +22,91 @@ INCLUDE_ASM("asm/nonmatchings/Overlays/walbosb/walbosb", WALBOSB_Collide);
 
 INCLUDE_ASM("asm/nonmatchings/Overlays/walbosb/walbosb", WALBOSB_Query);
 
-INCLUDE_ASM("asm/nonmatchings/Overlays/walbosb/walbosb", WALBOSB_Message);
+void WALBOSB_Message(Instance *instance, unsigned long message, unsigned long data)
+{
+
+    MonsterVars *mv; // not from debug symbols
+
+    mv = (MonsterVars *)instance->extraData;
+
+    switch (message)
+    {
+    case 0x400000:
+        if (data != 0)
+        {
+            SOUND_Play3dSound(&instance->position, 21, 650, 80, 15500);
+        }
+        MonsterMessage(instance, message, data);
+        break;
+    case 0x100001F:
+    case 0x1000021:
+        ((evMonsterHitData *)data)->power = 4096;
+        MonsterMessage(instance, message, data);
+        break;
+    case 0x1000020:
+        if (!(mv->mvFlags & 4))
+        {
+            if (instance->currentMainState != MONSTER_STATE_DEAD && instance->currentMainState != MONSTER_STATE_GENERALDEATH)
+            {
+                MON_SwitchStateDoEntry(instance, MONSTER_STATE_IDLE);
+                break;
+            }
+        }
+        break;
+    case 0x1000017:
+        switch (data)
+        {
+        case 0:
+            if (instance->currentMainState != MONSTER_STATE_GENERALDEATH && instance->currentMainState != MONSTER_STATE_DEAD)
+            {
+                mv->auxFlags |= 4;
+                MON_SwitchStateDoEntry(instance, MONSTER_STATE_DEAD);
+            }
+            break;
+        case 1:
+            if (instance->currentMainState != MONSTER_STATE_GENERALDEATH && instance->currentMainState != MONSTER_STATE_DEAD)
+            {
+                mv->auxFlags |= 1;
+                MON_SwitchStateDoEntry(instance, MONSTER_STATE_HIT);
+            }
+            break;
+        case 2:
+            if (instance->currentMainState != MONSTER_STATE_GENERALDEATH && instance->currentMainState != MONSTER_STATE_DEAD)
+            {
+                mv->auxFlags &= ~1;
+                MON_SwitchStateDoEntry(instance, MONSTER_STATE_COMBAT);
+            }
+            break;
+        case 3:
+            if (!(mv->auxFlags & 2))
+            {
+                mv->auxFlags |= 2;
+                MON_SwitchStateDoEntry(instance, MONSTER_STATE_IDLE);
+            }
+            else
+            {
+                mv->auxFlags &= ~2;
+            }
+            break;
+        case 4:
+            mv->auxFlags &= ~2;
+            break;
+        default:
+            break;
+        }
+        break;
+    case 0x40009:
+    case 0x80001:
+    case 0x80002:
+    case 0x80003:
+    case 0x80004:
+    case 0x80005:
+    case 0x80006:
+        break;
+    default:
+        MonsterMessage(instance, message, data);
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/Overlays/walbosb/walbosb", WALBOSB_Init);
 
@@ -45,7 +132,7 @@ INCLUDE_ASM("asm/nonmatchings/Overlays/walbosb/walbosb", WALBOSB_Hit);
 
 INCLUDE_ASM("asm/nonmatchings/Overlays/walbosb/walbosb", WALBOSB_DoNothingEntry);
 
-void WALBOSB_DoNothing() { };
+void WALBOSB_DoNothing() {};
 
 INCLUDE_ASM("asm/nonmatchings/Overlays/walbosb/walbosb", WALBOSB_GeneralDeathEntry);
 
@@ -79,52 +166,136 @@ INCLUDE_RODATA("asm/nonmatchings/Overlays/walbosb/walbosb", func_880006DC);
 
 #else 
 
-void WALBOSB_WalbossMessage(void) { };     
+void WALBOSB_WalbossMessage(void) {};
 
-void WALBOSB_AutofaceMarker(void) { };    
+void WALBOSB_AutofaceMarker(void) {};
 
-void WALBOSB_SetAutofacePos(void) { };    
+void WALBOSB_SetAutofacePos(void) {};
 
-void WALBOSB_ResetSetAutofacePos(void) { };
+void WALBOSB_ResetSetAutofacePos(void) {};
 
-void WALBOSB_Collide(void) { };    
+void WALBOSB_Collide(void) {};
 
-void WALBOSB_Query(void) { };   
+void WALBOSB_Query(void) {};
 
-void WALBOSB_Message(void) { };   
+void WALBOSB_Message(Instance *instance, unsigned long message, unsigned long data)
+{
 
-void WALBOSB_Init(void) { };      
+    MonsterVars *mv; // not from debug symbols
 
-void WALBOSB_CleanUp(void) { };     
+    mv = (MonsterVars *)instance->extraData;
 
-void WALBOSB_IdleEntry(void) { };    
+    switch (message)
+    {
+    case 0x400000:
+        if (data != 0)
+        {
+            SOUND_Play3dSound(&instance->position, 21, 650, 80, 15500);
+        }
+        MonsterMessage(instance, message, data);
+        break;
+    case 0x100001F:
+    case 0x1000021:
+        ((evMonsterHitData *)data)->power = 4096;
+        MonsterMessage(instance, message, data);
+        break;
+    case 0x1000020:
+        if (!(mv->mvFlags & 4))
+        {
+            if (instance->currentMainState != MONSTER_STATE_DEAD && instance->currentMainState != MONSTER_STATE_GENERALDEATH)
+            {
+                MON_SwitchStateDoEntry(instance, MONSTER_STATE_IDLE);
+                break;
+            }
+        }
+        break;
+    case 0x1000017:
+        switch (data)
+        {
+        case 0:
+            if (instance->currentMainState != MONSTER_STATE_GENERALDEATH && instance->currentMainState != MONSTER_STATE_DEAD)
+            {
+                mv->auxFlags |= 4;
+                MON_SwitchStateDoEntry(instance, MONSTER_STATE_DEAD);
+            }
+            break;
+        case 1:
+            if (instance->currentMainState != MONSTER_STATE_GENERALDEATH && instance->currentMainState != MONSTER_STATE_DEAD)
+            {
+                mv->auxFlags |= 1;
+                MON_SwitchStateDoEntry(instance, MONSTER_STATE_HIT);
+            }
+            break;
+        case 2:
+            if (instance->currentMainState != MONSTER_STATE_GENERALDEATH && instance->currentMainState != MONSTER_STATE_DEAD)
+            {
+                mv->auxFlags &= ~1;
+                MON_SwitchStateDoEntry(instance, MONSTER_STATE_COMBAT);
+            }
+            break;
+        case 3:
+            if (!(mv->auxFlags & 2))
+            {
+                mv->auxFlags |= 2;
+                MON_SwitchStateDoEntry(instance, MONSTER_STATE_IDLE);
+            }
+            else
+            {
+                mv->auxFlags &= ~2;
+            }
+            break;
+        case 4:
+            mv->auxFlags &= ~2;
+            break;
+        default:
+            break;
+        }
+        break;
+    case 0x40009:
+    case 0x80001:
+    case 0x80002:
+    case 0x80003:
+    case 0x80004:
+    case 0x80005:
+    case 0x80006:
+        break;
+    default:
+        MonsterMessage(instance, message, data);
+    }
+}
 
-void WALBOSB_HandleFade(void) { };   
+void WALBOSB_Init(void) {};
 
-void WALBOSB_Idle(void) { };         
+void WALBOSB_CleanUp(void) {};
 
-void WALBOSB_CombatEntry(void) { };    
+void WALBOSB_IdleEntry(void) {};
 
-void WALBOSB_Combat(void) { };        
+void WALBOSB_HandleFade(void) {};
 
-void WALBOSB_AttackEntry(void) { };      
+void WALBOSB_Idle(void) {};
 
-void WALBOSB_Attack(void) { };       
+void WALBOSB_CombatEntry(void) {};
 
-void WALBOSB_HitEntry(void) { };         
+void WALBOSB_Combat(void) {};
 
-void WALBOSB_Hit(void) { };             
+void WALBOSB_AttackEntry(void) {};
 
-void WALBOSB_DoNothingEntry(void) { };     
+void WALBOSB_Attack(void) {};
 
-void WALBOSB_DoNothing(void) { };          
+void WALBOSB_HitEntry(void) {};
 
-void WALBOSB_GeneralDeathEntry(void) { };  
+void WALBOSB_Hit(void) {};
 
-void WALBOSB_GeneralDeath(void) { };    
+void WALBOSB_DoNothingEntry(void) {};
 
-void WALBOSB_DeadEntry(void) { };     
+void WALBOSB_DoNothing(void) {};
 
-void WALBOSB_Dead(void) { };               
+void WALBOSB_GeneralDeathEntry(void) {};
+
+void WALBOSB_GeneralDeath(void) {};
+
+void WALBOSB_DeadEntry(void) {};
+
+void WALBOSB_Dead(void) {};
 
 #endif
